655727aca2f5fbf955f2f40328adbe1f9bd5ad79
Paul Okello <paulokello273@gmail.com>
2020-12-22 20:03:22 +0300

    Debugging 🙂🙂🙂

--- djangodb/website/views.py
+++ djangodb/website/views.py
@@ -5,3 +5,6 @@ from .models import Member
 def home(request):
     all_members = Member.objects.all
     return render(request, "home.html",{"all":all_members})
+
+def join(request):
+    return render(request, "join.html",{})
