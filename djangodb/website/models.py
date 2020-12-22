22cb3d5fac1e0b156f041990517d0e4d83058047
Paul Okello <paulokello273@gmail.com>
2020-12-22 17:44:53 +0300

    First Model ➖➖➖

--- djangodb/website/models.py
+++ djangodb/website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Member(models.Model):
+    fname = models.CharField( max_length=50)
+    lname = models.CharField( max_length=50)
+    password = models.CharField( max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField( max_length=254)
